e6936ba039dd23e04908e427dc78dad9d8df0123
Timafejn, Yuri <timafejn@gmail.com>
2023-07-05 08:55:44 -0300

    Criação da entidade 'Task' para gerenciar as tarefas

---
+++ src/entities/Task.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace entities;
+
+use DateTimeImmutable;
+
+class Task
+{
+    private ?int $id;
+    private ?DateTimeImmutable $dataStart;
+    private ?DateTimeImmutable $dateFinished;
+    private string $description;
+    private ?string $locale;
+    private ?string $notes;
+    private bool $flagFinish;
+
+    public const FLAG_NOT_FINISH = false;
+    public const FLAG_FINISH = true;
+
+    public function __construct(string $dataStart,
+                                string $description,
+                                ?string $locale,
+                                ?string $notes,
+                                ?int $id = null,
+                                bool $flagConcluido = self::FLAG_NOT_FINISH,
+                                ?string $dateFinished = null)
+    {
+        $this->id = ($id === null) ? null : $id;
+        $this->dataStart = new DateTimeImmutable($dataStart);
+        $this->description = $description;
+        $this->locale = $locale;
+        $this->notes = $notes;
+        $this->flagFinish = $flagConcluido;
+        $this->dateFinished = ($dateFinished !== null) ? new DateTimeImmutable($dateFinished) : null;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): Task
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getDataStart(): DateTimeImmutable
+    {
+        return $this->dataStart;
+    }
+
+    public function getDateFinished(): DateTimeImmutable
+    {
+        return $this->dateFinished;
+    }
+
+    public function setDateFinished(DateTimeImmutable $dateFinished): Task
+    {
+        $this->dateFinished = $dateFinished;
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): Task
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): Task
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+
+    public function getNotes(): string
+    {
+        return $this->notes;
+    }
+
+    public function setNotes(string $notes): Task
+    {
+        $this->notes = $notes;
+        return $this;
+    }
+
+    public function isFlagFinish(): bool
+    {
+        return $this->flagFinish;
+    }
+
+    public function finishTask(): Task
+    {
+        $this->flagFinish = self::FLAG_FINISH;
+        return $this;
+    }
+}
